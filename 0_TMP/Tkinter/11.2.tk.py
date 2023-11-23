4f4ea425c76ee653a90b5aa722b0d6d89c8d8eaa
cybsant <cybsant@gmail.com>
2023-11-23 17:56:49 +0300

    moved

---
+++ 0_TMP/Tkinter/11.2.tk.py
@@ -0,0 +1,136 @@
+import tkinter as tk
+from tkinter import messagebox
+import collections
+
+class PetApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("VetPet")
+        self.pets = collections.OrderedDict()
+
+        # Создаем верхний и нижний фреймы
+        top_frame = tk.Frame(master)
+        mod_frame = tk.Frame(master)
+        bottom_frame = tk.Frame(master)
+        top_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=False)
+        mod_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=False)
+        bottom_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        """ ВВОД """
+        # Вид
+        tk.Label(top_frame, text="[ Вид ]").grid(row=0, column=0, padx=5, sticky="e")
+        self.subj_entry = tk.Entry(top_frame)
+        self.subj_entry.grid(row=0, column=1, ipady=3, sticky="ew")
+        # Кличка
+        tk.Label(top_frame, text="[ Кличка ]").grid(row=1, column=0, padx=5, sticky="e")
+        self.pet_name_entry = tk.Entry(top_frame)
+        self.pet_name_entry.grid(row=1, column=1, ipady=3, sticky="ew")
+        # Возраст
+        tk.Label(top_frame, text="[ Возраст ]").grid(row=2, column=0, padx=5, sticky="e")
+        self.age_entry = tk.Entry(top_frame)
+        self.age_entry.grid(row=2, column=1, ipady=3, sticky="ew")
+        # Владелец
+        tk.Label(top_frame, text="[ Владелец ]").grid(row=3, column=0, padx=5, sticky="e")
+        self.owner_entry = tk.Entry(top_frame)
+        self.owner_entry.grid(row=3, column=1, ipady=3, sticky="ew")
+        # Кнопка Добавить
+        tk.Button(top_frame, text="Добавить", command=self.create).grid(row=3, column=3, sticky="ew")
+        # Фиксированный размер top_frame
+        top_frame.grid_rowconfigure(0, weight=1)
+        top_frame.grid_rowconfigure(1, weight=1)
+        top_frame.grid_rowconfigure(2, weight=1)
+        top_frame.grid_rowconfigure(3, weight=1)
+        top_frame.grid_rowconfigure(4, weight=1)
+        top_frame.grid_columnconfigure(1, weight=1)
+
+        """ ПРАВКА """
+        # Выбор ID
+        tk.Label(mod_frame, text="[ Выбор ID ]").grid(row=0, column=0, padx=5, sticky="w")
+        self.pet_ID_entry = tk.Entry(mod_frame)
+        self.pet_ID_entry.grid(row=0, column=1, padx=0, ipady=3, sticky="ew")
+        # Кнопки правки
+        tk.Button(mod_frame, text="Найти", command=self.read).grid(row=0, column=2, sticky="ew")
+        tk.Button(mod_frame, text="Правка", command=self.update).grid(row=0, column=3, sticky="ew")
+        tk.Button(mod_frame, text="Удалить", command=self.delete).grid(row=0, column=4, sticky="ew")
+        # ХЗ, пригодится
+        tk.Label(mod_frame, text="").grid(row=0, column=5, padx=40, sticky="w")
+        # Кнопка списка
+        tk.Button(mod_frame, text="Список", command=self.show_list).grid(row=0, column=6, sticky="e")
+
+        """ СПИСОК """
+        # Cписок питомцев и его скроллбар
+        self.text_area = tk.Text(bottom_frame)
+        self.text_area.grid(row=0, column=0, sticky="nsew")
+        self.scroll = tk.Scrollbar(bottom_frame)
+        self.scroll.grid(row=0, column=1, sticky="ns")
+        # Привязка скролл-бара к текстовому полю
+        self.text_area.config(yscrollcommand=self.scroll.set)
+        self.scroll.config(command=self.text_area.yview)
+        # Растяжение фрейма bottom_frame
+        bottom_frame.grid_rowconfigure(0, weight=1)
+        bottom_frame.grid_columnconfigure(0, weight=1)
+
+    def show_list(self):
+        self.text_area.delete(1.0, tk.END)
+        for ID, pet_info in self.pets.items():
+            self.text_area.insert(tk.END, f'{ID}. {pet_info["Вид питомца"]} \
+                по кличке {pet_info["pet_name"]}. \
+                Возраст: {pet_info["Возраст питомца"]}.\
+                Владелец: {pet_info["Имя владельца"]}\n'
+            )
+
+    def create(self):
+        # обработка возраста
+        try:
+            age = int(self.age_entry.get())
+        except ValueError:
+            messagebox.showerror("Ошибка", "Пожалуйста, введите число для возраста.")
+            return
+        # создание записи о питомце
+        ID = max(self.pets.keys(), default=0) + 1
+        self.pets[ID] = {
+            "pet_name": self.pet_name_entry.get(),
+            "Вид питомца": self.subj_entry.get(),
+            "Возраст питомца": age,
+            "Имя владельца": self.owner_entry.get()
+        }
+
+    def delete(self):
+        ID = self.pet_ID_entry.get()
+        if ID in self.pets:
+            del self.pets[ID]
+            messagebox.showerror(f"<!> {ID} удален <!>")
+        else:
+            messagebox.showerror("Ошибка!", "Питомец с таким ID не найден")
+
+    def read(self):
+        ID = self.pet_ID_entry.get()
+        if ID in self.pets:
+            self.text_area.delete(1.0, tk.END)
+            pet_info = self.pets[ID]
+            self.text_area.insert(tk.END, f' {ID}. {pet_info["Вид питомца"]} \
+                по кличке {pet_info["pet_name"]}. \
+                Возраст: {pet_info["Возраст питомца"]}. \
+                Владелец: {pet_info["Имя владельца"]}\n'
+            )
+        else:
+            messagebox.showerror("Ошибка!", "Питомец с таким ID не найден")
+
+    def update(self):
+        ID = self.pet_ID_entry.get()
+        if ID in self.pets:
+            del self.pets[ID]
+            self.pets[ID] = {
+                "pet_name": self.pet_name_entry.get(),
+                "Вид питомца": self.subj_entry.get(),
+                "Возраст питомца": self.age_entry.get(),
+                "Имя владельца": self.owner_entry.get()
+            }
+        else:
+            messagebox.showerror("Ошибка!", "Питомец с таким ID не найден")
+
+# ---[ S T A R T ]---
+root = tk.Tk()
+root.geometry("720x600")
+app = PetApp(root)
+root.mainloop()
---
+++ 0_TMP/Tkinter/calc.py
@@ -0,0 +1,128 @@
+import tkinter as tk
+from tkinter import ttk
+from decimal import Decimal
+
+class Calculator(ttk.Frame):
+    def __init__(self):
+        ttk.Frame.__init__(self)
+        self.active_str = ''
+        self.stack = []
+
+    def click_number(self, number):
+        self.active_str += number
+        return self.active_str
+
+    def click_decimal_point(self):
+        if self.active_str.find('.') == -1:
+            self.active_str += '.'
+        return self.active_str
+
+    def clear(self):
+        self.stack.clear()
+        self.active_str = ''
+        return '0'
+
+    def calculate(self):
+        try:
+            self.stack.append(self.active_str)
+            result = self._perform_calculation()
+            self.stack.clear()
+            self.active_str = str(result)
+            return result
+        except ZeroDivisionError:
+            return 'Error: Division by zero'
+        except:
+            return 'Error: Calculation error'
+
+    def _perform_calculation(self):
+        result = Decimal(self.stack[0])
+
+        for i in range(1, len(self.stack), 2):
+            operation = self.stack[i]
+            operand = Decimal(self.stack[i + 1])
+
+            if operation == '+':
+                result += operand
+            elif operation == '-':
+                result -= operand
+            elif operation == '*':
+                result *= operand
+            elif operation == '/':
+                if operand != 0:
+                    result /= operand
+                else:
+                    raise ZeroDivisionError("Division by zero")
+            else:
+                raise ValueError("Invalid operation")
+
+        return result
+
+
+class CalculatorUI:
+    def __init__(self, root, calculator):
+        self.calculator = calculator
+        self.root = root
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.root.title('Calculator')
+        buttons = (('1', '2', '3', '/'),
+                   ('4', '5', '6', '*'),
+                   ('7', '8', '9', '-'),
+                   ('.', '0', '=', '+')
+                   )
+
+        ttk.Style().theme_use("clam")
+        label_style = ttk.Style()
+        label_style.configure("My.TLabel",
+                              font="helvetica 14",
+                              foreground="#004D40",
+                              padding=4,
+                              background="#B2DFDB")
+
+        frame = ttk.Frame(self.root)
+        frame.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=2, pady=2)
+
+        self.label = ttk.Label(frame, text='0', width=18, style="My.TLabel")
+        self.label.grid(row=0, column=0, sticky="w", padx=3, pady=2)
+
+        button = ttk.Button(frame, text='CE', command=self.click_ce, width=6)
+        button.grid(row=0, column=1, sticky="e")
+
+        for row in range(4):
+            for col in range(4):
+                button_text = buttons[row][col]
+                button = ttk.Button(self.root,
+                                    text=button_text,
+                                    command=lambda text=button_text: self.click(text),
+                                    width=2)
+                button.grid(row=row + 1, column=col, sticky="nsew", padx=2, pady=2)
+
+    def click(self, text):
+        if text == '=':
+            result = self.calculator.calculate()
+            self.label.configure(text=str(result))
+            return result
+        elif '0' <= text <= '9':
+            result = self.calculator.click_number(text)
+            self.label.configure(text=result)
+            return result
+        elif text == '.':
+            result = self.calculator.click_decimal_point()
+            self.label.configure(text=result)
+            return result
+
+        self.calculator.stack.append(self.calculator.active_str)
+        self.calculator.stack.append(text)
+        self.calculator.active_str = ''
+        return self.calculator.active_str
+
+    def click_ce(self):
+        result = self.calculator.clear()
+        self.label.configure(text=result)
+
+# ---[ S T A R T ]---
+#root = ()
+calculator = Calculator()
+CalculatorUI(root, calculator)
+root.mainloop()
